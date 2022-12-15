f83d8279451084ed7fb9eab2656d3149efddf90f
a22nicolesdc <a22nicolesdc@192.168.56.1>
2022-12-15 12:58:15 +0100

    casiFinalizadaLaBaseDeDatos

--- src/main/java/com/mycompany/caralibro/CaraLibroBD.java
+++ src/main/java/com/mycompany/caralibro/CaraLibroBD.java
@@ -4,37 +4,55 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CaraLibroBD {
-    Scanner sc1 = new Scanner(System.in);
-    ArrayList <String> perfiles = new ArrayList<>();
-    
-    
-    public void obtenerPerfil(String nombre, String contraseña){
+
+    Scanner entrada = new Scanner(System.in);
+    ArrayList<String> perfiles = new ArrayList<>();
+
+    public void obtenerPerfil(String nombre, String contraseña) {
         String contr = contraseña;
         String nombr = nombre;
         boolean entrar = false;
-        if(!perfiles.isEmpty()){
-            while(!entrar){
-                for(int i = 0; i < perfiles.size();i++){
-                    
-                    if(perfiles.get(i).contains(nombr) && perfiles.get(i).contains(contr)){
+        if (!perfiles.isEmpty()) {
+            while (!entrar) {
+                for (int i = 0; i < perfiles.size(); i++) {
+
+                    if (perfiles.get(i).contains(nombr) && perfiles.get(i).contains(contr)) {
                         perfiles.get(i);
                         entrar = true;
                     }
                 }
-                
+
             }
-        }
+        }else
+            System.out.print("No existe el usuario");
+            
+            
     }
 
-    public void buscarPerfil(){
+    public void buscarPerfil(Perfil nombre) {
+        
+    
+        for (int i = 0; i < perfiles.size(); i++) {
+
+            if (perfiles.contains(nombre)){
+                
+                   System.out.print("El perfil si existe");
+                   
+               
+            }else{
+                System.out.print("El perfil no existe");
+            }
+
+        }
+        
+            
         
     }
 
-    public void añadirPerfil(){
-        for(int i = 0; i < caraLibroBD.size();i++){
-            System.out.println("Introduce o teu nome");
-            
-        }
+    public void añadirPerfil() {
+       
     }
+
+    
 
 }
