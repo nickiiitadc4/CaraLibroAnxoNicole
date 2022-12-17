01942cf02bdd09ff90b3ac5d57f491f0a3b28b7a
a22nicolesdc <a22nicolesdc@192.168.56.1>
2022-12-17 13:45:57 +0100

    perfil solo queda 1 cosa

--- src/main/java/com/mycompany/caralibro/Mensaje.java
+++ src/main/java/com/mycompany/caralibro/Mensaje.java
@@ -2,17 +2,33 @@ package com.mycompany.caralibro;
 
 import java.sql.Date;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class Mensaje {
     public LocalDateTime fecha;
     public String texto;
     public Boolean leido;
     Perfil remitente;
+    ArrayList<Mensaje> mensajes = new ArrayList <> (); 
     
+    public Mensaje(){
+        
+    }
     public Mensaje(String texto, Perfil remitente){
         this.texto = texto;
         this.fecha = LocalDateTime.now();
         this.remitente = remitente;
+        
+        for (int contador = 0 ; contador < mensajes.size(); contador ++){
+            
+            
+            mensajes.get(contador).fecha.isAfter(fecha);
+            mensajes.add(this);
+            
+        }
+        
+        
+            
     }
 
 }
